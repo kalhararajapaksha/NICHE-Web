c4bd4a8087658f9ee62826b4f5ec2c99746cdb00
kalhararajapaksha <kasunkrajapaksha@gmail.com>
2021-08-09 09:49:23 +0530

    Many changes

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+      <Layout pageHeading='About Page' pageTitle='about Page'>
+       Welcome to my Gatsby site about page! 
+      </Layout>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from '../components/layout'
+
+const ContactPage = () => {
+  return (
+      <Layout pageHeading='Contact Page' pageTitle='Contact Page'>
+       Welcome to my Gatsby site contact page! 
+      </Layout>
+  )
+}
+
+export default ContactPage
